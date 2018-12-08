0fa59d29cd3dfb545a2a6a82f63672234d580923
yukinorei <>
2018-12-08 13:11:36 +0900

    controller.jsの修正

--- server/controller.js
+++ server/controller.js
@@ -3,8 +3,11 @@ const dog = require('./dog');
 const fetchDogById = (req, res) => {
   // myParams は middleware の parsePathForParameters関数内で定義されます。
   const { id } = req.myParams;
+  if (typeof id !== 'number') {
+    return res.status(400).json({});
+  }
   const dogObj = dog.getDogById(id);
-  if ((typeof id !== 'number') || !dogObj) {
+  if (!dogObj) {
     return res.status(400).json({});
   }
   return res.json(dogObj);
